47b9442cd71179973e2dbb6f4f33a86fc5bc9871
Informatica Arelux <informatica@arelux.com>
2020-05-06 13:20:19 +0200

    upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Automation Log',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/automation_view.xml',
+    ],    
+    'installable': True,
+    'auto_install': False,    
+}
