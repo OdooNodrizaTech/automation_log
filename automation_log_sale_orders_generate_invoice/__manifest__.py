cf17665b46cc4464ffe213897ab2a2e493dd83bc
Informatica Arelux <informatica@arelux.com>
2020-07-03 09:09:33 +0200

    # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).

--- automation_log_sale_orders_generate_invoice/__manifest__.py
+++ automation_log_sale_orders_generate_invoice/__manifest__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Automation Log Sale Orders Generate Invoice',
     'version': '12.0.1.0.0',    
--- automation_log_survey_arelux/models/__init__.py
+++ automation_log_survey_arelux/models/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import survey_mail_compose_message
